9d97ee5593fd83701a40c5f684ed8f68eba4b4f2
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-06-20 14:46:14 +0800

    编写dao层

--- sportplay/sport/src/main/java/com/jinxiao/sport/SportApplication.java
+++ sportplay/sport/src/main/java/com/jinxiao/sport/SportApplication.java
@@ -1,8 +1,10 @@
 package com.jinxiao.sport;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.jinxiao.sport.dao")
 @SpringBootApplication
 public class SportApplication {
 
--- sportplay/sport/src/main/java/com/jinxiao/sport/controller/LoginController.java
+++ sportplay/sport/src/main/java/com/jinxiao/sport/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.jinxiao.sport.controller;
 
 import com.jinxiao.sport.bean.User;
+import com.jinxiao.sport.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    @Autowired
+    UserDao userDao;
+
     @RequestMapping("/login")
     public String login(@RequestBody User user) {
-        System.out.println("user: " + user);
+        User us = userDao.getUserByMessage(user.getUsername(), user.getPassword());
+        System.out.println("user: " + us);
+        
         return "ok";
     }
 }
