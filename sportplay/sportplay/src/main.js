58dc5211dc608f041eec036e305370c9911f6850
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-06-20 14:03:13 +0800

    完成登录和重置事件

--- sportplay/sportplay/src/main.js
+++ sportplay/sportplay/src/main.js
@@ -5,6 +5,14 @@ import './plugins/element.js'
 import './assets/css/global.css' // 添加全局样式
 import './assets/font/iconfont.css' // 引入iconfont
 
+// 导入axios
+import  axios from 'axios'
+// 挂载axios
+Vue.prototype.$http = axios
+// 设置访问根路径
+axios.defaults.baseURL = "http://localhost:9000"
+
+
 Vue.config.productionTip = false
 
 new Vue({
