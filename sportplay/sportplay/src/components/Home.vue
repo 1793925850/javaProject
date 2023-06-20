218c00e015cc439cef55c6d3f17623f21d443ae9
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-06-20 19:49:54 +0800

    布局和header

--- sportplay/sportplay/src/components/Home.vue
+++ sportplay/sportplay/src/components/Home.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>Home!
-        <el-button type="info" @click="logout">安全退出</el-button>
-    </div>
+  <!--引入container布局-->
+    <el-container class="home-container">
+        <!--头部-->
+        <el-header>
+            <div>
+                <img src="../assets/logo.png" alt="">
+                <span>个人运动平台</span>
+            </div>
+            <el-button type="info" @click="logout">安全退出</el-button>
+        </el-header>
+        <!--主体-->
+        <el-container>
+            <!--侧边栏-->
+            <el-aside width="200px">Aside</el-aside>
+            <!--主体内容-->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 <script>
 export default {
@@ -15,5 +30,43 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+// 头样式
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between; // 左右贴边
+  padding-left: 0%; // 左边界
+  color: #fff;
+  font-size: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-left: 15px;
+    }
+  }
+
+}
+
+// 侧边样式
+.el-aside {
+  background-color: #333744;
+}
+
+// 主体样式
+.el-main {
+  background-color: #eaedf1;
+}
+
+img {
+  width: 55px;
+  height: 55px;
+}
 
 </style>
